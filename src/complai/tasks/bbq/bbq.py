283cc40748279517eb38de24c525c908274f2485
hehli <hehli@protonmail.com>
2025-08-29 07:38:09 +0000

    Add bbq task args

--- src/complai/tasks/bbq/bbq.py
+++ src/complai/tasks/bbq/bbq.py
@@ -1,8 +1,11 @@
 from inspect_ai import Task
 from inspect_ai import task
 from inspect_evals.bbq import bbq
+from inspect_evals.bbq import BBQSubset
 
 
 @task(name="bbq", technical_requirement="Representation — Absence of Bias")
-def wrapped_bbq() -> Task:
-    return bbq()
+def wrapped_bbq(
+    subsets: BBQSubset | list[BBQSubset] | None = None, shuffle: bool = False
+) -> Task:
+    return bbq(subsets=subsets, shuffle=shuffle)
