43dd30eb7007c639dbe0570e0141e376e60a7206
hehli <hehli@protonmail.com>
2025-09-03 14:32:21 +0000

    Use "mc2" targets

--- src/complai/tasks/truthfulqa/truthfulqa.py
+++ src/complai/tasks/truthfulqa/truthfulqa.py
@@ -1,3 +1,5 @@
+from typing import Literal
+
 from inspect_ai import Task
 from inspect_ai import task
 from inspect_evals.truthfulqa import truthfulqa
@@ -7,5 +9,5 @@ from inspect_evals.truthfulqa import truthfulqa
     name="truthfulqa",
     technical_requirement="Capabilities, Performance, and Limitations",
 )
-def wrapped_truthfulqa() -> Task:
-    return truthfulqa()
+def wrapped_truthfulqa(target: Literal["mc1", "mc2"] = "mc2") -> Task:
+    return truthfulqa(target)
