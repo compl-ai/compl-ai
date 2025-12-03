cc7a3710a4076b8a801143ea3c1126acb160afa3
Johannes Zschache <johannes.zschache@uba.de>
2025-12-03 12:10:22 +0100

    adding options for custom grader model

--- src/complai/tasks/mask/mask.py
+++ src/complai/tasks/mask/mask.py
@@ -4,5 +4,10 @@ from inspect_evals.mask import mask as inspect_mask
 
 
 @task(technical_requirement="Societal Alignment")
-def mask() -> Task:
-    return inspect_mask()
+def mask(
+    binary_judge_model: str = "openai/gpt-4o",
+    numeric_judge_model: str = "openai/o3-mini",
+) -> Task:
+    return inspect_mask(
+        binary_judge_model=binary_judge_model, numeric_judge_model=numeric_judge_model
+    )
