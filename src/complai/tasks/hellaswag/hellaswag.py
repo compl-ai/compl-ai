c953b3686814d4f4b290de1de614d43bbf8b3d8f
hehli <75807102+hehli@users.noreply.github.com>
2025-12-08 16:22:44 +0100

    Remove hellaswag (#113)
    Replaced by GPQA Diamond

--- src/complai/tasks/hellaswag/hellaswag.py
+++
@@ -1,8 +0,0 @@
-from inspect_ai import Task
-from inspect_ai import task
-from inspect_evals.hellaswag import hellaswag as inspect_hellaswag
-
-
-@task(technical_requirement="Capabilities, Performance, and Limitations")
-def hellaswag() -> Task:
-    return inspect_hellaswag()
