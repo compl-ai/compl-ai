8e40fe912025ea7add9612c4a0309e1e05d699dd
hehli <hehli@protonmail.com>
2025-12-08 16:24:03 +0100

    Add `technical_requirement` attr to hle, ifbench, truthfulqa

--- src/complai/tasks/truthfulqa/truthfulqa.py
+++ src/complai/tasks/truthfulqa/truthfulqa.py
@@ -5,6 +5,6 @@ from inspect_ai import task
 from inspect_evals.truthfulqa import truthfulqa as inspect_truthfulqa
 
 
-@task(technical_requirement="Capabilities, Performance, and Limitations")
+@task(technical_requirement="Societal Alignment")
 def truthfulqa(target: Literal["mc1", "mc2"] = "mc2") -> Task:
     return inspect_truthfulqa(target)
