2427188b509beaad6a829d7c0bf9647275fd78d7
hehli <hehli@protonmail.com>
2026-07-13 20:02:04 +0200

    Add `samples` command

--- src/complai/_cli/__init__.py
+++ src/complai/_cli/__init__.py
@@ -3,12 +3,14 @@ from inspect_ai._util.dotenv import init_dotenv
 
 from complai._cli.eval import eval_command
 from complai._cli.list import list_command
+from complai._cli.samples import samples_command
 
 
 app = typer.Typer(rich_markup_mode="markdown")
 
 app.command("eval")(eval_command)
 app.command("list")(list_command)
+app.command("samples")(samples_command)
 
 
 def main() -> None:
